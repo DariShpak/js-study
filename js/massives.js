db2e1b4efc851ca17d7cf6b4fc7e0dde5d49df09
Daria Shpak <darya.shpak@gmail.com>
2023-04-01 17:39:32 +0300

    new tasks solved

--- js/massives.js
+++ js/massives.js
@@ -278,16 +278,181 @@
 // console.log(createArrayOfNumbers(14, 17))
 // console.log(createArrayOfNumbers(29, 34))
 
-function filterArray(numbers, value) {
-  // Change code below this line
-  for (let i = 0; i <= numbers.length; i += 1)
-    if (i > value) {
-  numbers.push(i)
-}
+// const apartment = {
+//   descr: "Spacious apartment in the city center",
+//   rating: 4,
+//   price: 2153,
+// };
+// const keys = [];
+// const values = [];
+// // Change code below this line
+// for (const key in apartment){
+//   keys.push(key)
+//   values.push(apartment[key])
+// }
+// console.log(keys)
+//   console.log(values)
+
+// const keys = []
+// const values = []
+// const advert = {
+//   service: "apt",
+// }
+// const apartment = Object.create(advert)
+// apartment.descr = "Spacious apartment in the city center"
+// apartment.rating = 4
+// apartment.price = 2153
+
+// for (const key in apartment) {
+//   // Change code below this line
+//   if (apartment.hasOwnProperty(key)){
+//     keys.push(key);
+//     values.push(apartment[key]);
+//   }
+
+//     }
+//   // Change code above this line
+
+// console.log(keys);
+// console.log(values)
+
+// function countProps(object) {
+//   let propCount = 0
+//   // Change code below this line
+//   for (const key in object)
+//     if (object.hasOwnProperty(key))
+//     propCount += 1
+//   // Change code above this line
+//   return propCount
+// }
+// console.log(countProps({}))
+// console.log(countProps({ name: "Mango", age: 2 }))
+// console.log(countProps({mail: "poly@mail.com", isOnline: true, score: 500}))
+
+// const apartment = {
+//   descr: "Spacious apartment in the city center",
+//   rating: 4,
+//   price: 2153,
+// }
+// const values = []
+// // Change code below this line
+// const keys = Object.keys(apartment) 
+// console.log(keys)
+
+// for (const key of keys) {
+// values.push(apartment[key])
+// }
+
+// console.log(values)
+
+// function countProps(object) {
+//   // Change code below this line
+//   let propCount = 0
+//   let keysOfObject = Object.keys(object);
+
+//   for (const key of keysOfObject) {
+//   propCount += 1
+// }
+
+//   // for (const key in object) {
+//   //   if (object.hasOwnProperty(key)) {
+//   //     propCount += 1
+//   //   }
+//   // }
+
+
+
+//   return propCount
+//   // Change code above this line
+// }
+// console.log(countProps({}))
+// console.log(countProps({ name: "Mango", age: 2 }))
+// console.log(countProps({ mail: "poly@mail.com", isOnline: true, score: 500 }))
+
+// const apartment = {
+//   descr: "Spacious apartment in the city center",
+//   rating: 4,
+//   price: 2153,
+// }
+// // Change code below this line
+// const keys = Object.keys(apartment)
+// const values = Object.values(apartment)
+
+// console.log(keys)
+// console.log(values)
+
+// function countTotalSalary(salaries) {
+//   let totalSalary = 0
+//   // Change code below this line
+//   const valuesOfSalaries = Object.values(salaries);
+//   console.log(valuesOfSalaries)
+
+//   for (const value of valuesOfSalaries) {
+//     totalSalary += value
+//   }
+//   // Change code above this line
+//   return totalSalary
+// }
+// console.log(countTotalSalary({}))
+// console.log(countTotalSalary({ mango: 100, poly: 150, alfred: 80 }))
+// console.log(countTotalSalary({kiwi: 200, poly: 50, ajax: 150}))
+
+// const colors = [
+//   { hex: "#f44336", rgb: "244,67,54" },
+//   { hex: "#2196f3", rgb: "33,150,243" },
+//   { hex: "#4caf50", rgb: "76,175,80" },
+//   { hex: "#ffeb3b", rgb: "255,235,59" },
+// ];
+
+// const hexColors = [];
+// const rgbColors = [];
+
+// for (const color of colors) {
+//   hexColors.push(color.hex);
+//   rgbColors.push(color.rgb)
+
+// }
+
+// console.log(hexColors)
+// console.log(rgbColors)
+
+
+// const products = [
+//   {name: "Radar", price: 1300, quantity: 4},
+//   {name: "Scanner", price: 2700, quantity: 3},
+//   {name: "Droid", price: 400, quantity: 7},
+//   {name: "Grip", price: 1200, quantity: 9},
+// ]
+
   
-  // Change code above this line
+// function getProductPrice(productName) {
+
+
+//   for (const product of products) {
+//     if (product.name === productName) {
+//       return product.price;
+//     }
+//   }
+//     return null
+//   }
+
+// console.log(getProductPrice("Radar"))
+// console.log(getProductPrice("Grip"))
+// console.log(getProductPrice("Scanner"))
+// console.log(getProductPrice("Droid"))
+// console.log(getProductPrice("Engine"))
+
+const products = [
+  {name: "Radar", price: 1300, quantity: 4},
+  {name: "Scanner", price: 2700, quantity: 3},
+  {name: "Droid", price: 400, quantity: 7},
+  {name: "Grip", price: 1200, quantity: 9},
+]
+console.table(products)
+function getAllPropValues(propName) {
 }
-console.log(filterArray([1, 2, 3, 4, 5], 3))
-console.log(filterArray([1, 2, 3, 4, 5], 4))
-console.log(filterArray([1, 2, 3, 4, 5], 5))
-console.log(filterArray([12, 24, 8, 41, 76], 38))
+
+console.log(getAllPropValues("name"))
+console.log(getAllPropValues("quantity"))
+console.log(getAllPropValues("price"))
+console.log(getAllPropValues("category"))
